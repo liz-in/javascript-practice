0c4a052fda71a4d13666bc7088718c053e9aab25
Liz Yan <lizyan@blueplanet.com.tw>
2020-06-18 09:48:29 +0800

    feat: 首頁、猜數字遊戲頁面

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ var myImage = document.querySelector('img');
 myImage.onclick = function () {
     let imgSrc = myImage.getAttribute('src');
     if (imgSrc === 'assets/photo-ocean.jpg') {
-        myImage.setAttribute('src', 'assets/photo-farm.jpg')
+        myImage.setAttribute('src', 'assets/photo-farm.jpg');
     } else {
-        myImage.setAttribute('src', 'assets/photo-ocean.jpg')
+        myImage.setAttribute('src', 'assets/photo-ocean.jpg');
     }
 }
 
@@ -22,9 +22,12 @@ function setUserName() {
 if (localStorage.getItem('name')) {
     let storedName = localStorage.getItem('name');
     if (!storedName || storedName === null) {
-        setUserName()
+        setUserName();
     } else {
-        myTitle.innerHTML = storedName + ', nice to see you again! :) <br> Keywords are tokens that have special meaning in JavaScript: break, case, catch, continue, debugger, default, delete, do, else, finally, for, function, if, in, instanceof, new, return, switch, this, throw, try, typeof, var, void, while, and with.';
+        myTitle.innerHTML = storedName + ', nice to see you again! :)';
+        let paragraph = document.createElement('p');
+        paragraph.innerHTML = 'Story:<br><br>There was once a shoemaker, who worked very hard and was very honest, but still he could not earn enough to live upon; and at last all he had in the world was gone, save just leather enough to make one pair of shoes.Then he cut his leather out, all ready to make up the next day, meaning to rise early in the morning to his work. His conscience was clear and his heart light amidst all his troubles; so he went peaceably to bed, left all his cares to Heaven, and soon fell asleep.<br><br>In the morning after he had said his prayers, he sat himself down to his work; when, to his great wonder, there stood the shoes already made, upon the table. The good man knew not what to say or think at such an odd thing happening. He looked at the workmanship; there was not one false stitch in the whole job; all was so neat and true, that it was quite a masterpiece. The same day a customer came in, and the shoes suited him so well that he willingly paid a price higher than usual for them; and the poor shoemaker, with the money, bought leather enough to make two pairs more.<br><br>In the evening he cut out the work, and went to bed early, that he might get up and begin betimes next day; but he was saved all the trouble, for when he got up in the morning the work was done ready to his hand. Soon in came buyers, who paid him handsomely for his goods, so that he bought leather enough for four pair more. He cut out the work again overnight and found it done in the morning, as before; and so it went on for some time: what was got ready in the evening was always done by daybreak, and the good man soon became thriving and well off again.';
+        document.querySelector('.content').appendChild(paragraph);
     }
 } else {
     setUserName();
--- js/number-guess.js
+++ js/number-guess.js
@@ -6,6 +6,77 @@ const lowOrHi = document.querySelector('.lowOrHi');
 
 const btn = document.querySelector('.btn');
 const input = document.querySelector('#number');
+const msgblock = document.querySelector('.msg-block');
 
 let guessCount = 1;
 let resetBtn;
+
+function checkGuess() {
+    const userGuess = parseInt(input.value, 10);
+    if(isNaN(userGuess)) {
+        alert('Please enter a numerical');
+        input.value = '';
+        input.focus();
+        return false;
+    }
+
+    if (guessCount === 1) {
+        guesses.textContent = 'Previous guesses:';
+    }
+    guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You got it right';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = 'Game Over ...';
+        setGameOver();
+    } else {
+        lastResult.textContent = 'Wrong!';
+        lastResult.style.backgroundColor = 'red';
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Your number is LOWER than target number.';
+        } else {
+            lowOrHi.textContent = 'Your number is HIGHER than target number.';
+        }
+    }
+
+    guessCount++;
+    input.val = '';
+    input.focus();
+}
+
+btn.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    btn.disabled = true;
+
+    resetBtn = document.createElement('button');
+    resetBtn.textContent = 'Start new game';
+    msgblock.appendChild(resetBtn);
+    resetBtn.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+
+    var resetParagraph = document.querySelectorAll('.msg-block>p');
+    for (let i = 0; i < resetParagraph.length; i++) {
+        resetParagraph[i].textContent = '';
+        resetParagraph[i].style.backgroundColor = 'white';
+    }
+
+    resetBtn.parentNode.removeChild(resetBtn);
+
+    var resetButton = document.querySelectorAll('button');
+    for (let i = 0; i < resetButton.length; i++) {
+        resetButton[i].disabled = false;
+    }
+
+    input.disabled = false;
+    input.value = '';
+    input.focus();
+    randomNumber = Math.floor(Math.random * 100) + 1;
+}
